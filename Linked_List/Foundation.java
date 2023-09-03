16be0c1f10366d426d7b215f7fc9d3e654b00b56
shreemankumar <bshreemankumar@gmail.com>
2023-09-03 21:48:34 +0530

    updated

--- Linked_List/Foundation.java
+++ Linked_List/Foundation.java
@@ -1,23 +1,47 @@
+import javafx.scene.chart.PieChart.Data;
+
 class Node{
     int data;
     Node next;
+
+    public Node(int d){
+    this.data=d;
+
+    }
 }
 
 public class Foundation{
+    static Node head;
+   static  Node tail;
+
+    public static void addLast(int  data){
+        Node nn= new Node(data) ;
+         tail.next=nn;
+         tail=nn;
+    }
     public static void main(String[] args) {
-        Node f=new Node();
-        f.data=5;
+        
+        Node f=new Node(7);
+        
 
-        Node s= new Node();
-        s.data=4;
-
-        Node r= new Node();
-        r.data=12;
+        Node s= new Node(0);
+        
+        
+        Node r= new Node(1);
+        head=f;
+        tail=r;
+         
 
         //connecting frist and second   (Node@15db9742 )
         f.next=s;
         s.next=r;
-        System.out.println(s.next.data);
+       addLast(0);
+       addLast(45);
+       
+        System.out.println(tail.next.data);
+         
+        
+
         
     }
 }
