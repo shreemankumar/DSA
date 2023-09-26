495f81f880441e04f6f68dbdaae3d7ed162b99a1
shreemankumar <bshreemankumar@gmail.com>
2023-09-26 23:50:07 +0530

    dry run

--- Linked_List/Ragain.java
+++ Linked_List/Ragain.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 
+
+
 class Node{
     int data;
     Node next;
@@ -28,17 +30,73 @@ public class Ragain {
         }
        
     }
+    public static void Add_First(int data){
+        Node nn=new Node(data);
+        if(head==null){
+            head=nn;
+            tail=nn;
+        }
+        else{
+            nn.next=head;
+            head=nn;
+        }
+    }
+    public static void Display(){
+        Node curr=head;
+
+        while(curr!=null){
+            System.out.print(curr.data+" ");
+            curr=curr.next;
+            System.out.println();
+        }
+
+    }
+    public static void Remove_First(){
+        if(head!=null){
+          head= head.next;
+        }
+        //System.out.println();
+    }
+    public static Node Gets_At(int idx){
+        if(idx<0){
+            System.out.println("null");
+            return null;
+        }
+        Node curr=head;
+        for(int i=0;i<idx;i++){
+            if(curr==null){
+                System.out.println("null");
+                return null;
+            }
+            curr=curr.next;
+        }
+        return curr;
+    }
     public static void main(String[] args) {
-         
+       Scanner sc=new Scanner(System.in);  
+       System.out.println("Enter the size :");
+       int n=sc.nextInt();
          head =null;
          tail=null;
-         for(int i=0;i<=10;i++){
-            Add_Last(i);
-            System.out.println(head.data);
+         System.out.println("Enter the element :");
+         for(int i=0;i<n;i++){
+            int a =sc.nextInt();
+            Add_Last(a);
          }
+        //  Add_First(11111);
+        //  Add_First(12);
         
 
         // System.out.println(head.data);
+        // System.out.println(tail.data);
+
+         Display();
+        // Remove_First();
+        // Display();
+       Node ans= Gets_At(-1);
+       if(ans!=null)
+         System.out.println(ans.data);
+
     }
     
 }
