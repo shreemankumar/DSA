686a78343729ef697b84e8b2d10182ecbd44d93f
shreemankumar <bshreemankumar@gmail.com>
2023-10-30 15:30:45 +0530

    84(hard)

---
+++ Leetcode/stack/Largest_Rectangle _in_Histogram.java
@@ -0,0 +1,61 @@
+
+            // leetcode 84(hard)
+
+class Solution {
+    public int[] getNSERI(int[] arr, int n) {
+        int[] nseri = new int[n];
+        Stack<Integer> st = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            int ele = arr[i];
+
+            while (!st.isEmpty() && arr[st.peek()] > ele) {
+                nseri[st.peek()] = i;
+                st.pop();
+            }
+            st.push(i);
+        }
+        while (!st.isEmpty()) {
+            nseri[st.peek()] = n;
+            st.pop();
+        }
+        return nseri;
+    }
+
+    public int[] getNSELI(int[] arr, int n) {
+        int[] nseli = new int[n];
+        Stack<Integer> st = new Stack<>();
+
+        for (int i = n - 1; i >= 0; i--) {
+            int ele = arr[i];
+
+            while (!st.isEmpty() && arr[st.peek()] > ele) {
+                nseli[st.peek()] = i;
+                st.pop();
+            }
+            st.push(i);
+        }
+        while (!st.isEmpty()) {
+            nseli[st.peek()] = -1;
+            st.pop();
+        }
+        return nseli;
+    }
+
+    public int largestRectangleArea(int[] heights) {
+        int n = heights.length;
+
+        int[] nseli = getNSELI(heights, n);
+        int[] nseri = getNSERI(heights, n);
+
+        int maxarea = 0;
+        for (int i = 0; i < n; i++) {
+            int height = heights[i];
+            int width = nseri[i] - nseli[i] - 1;
+            int area = height * width;
+            maxarea = Math.max(maxarea, area);
+        }
+        return maxarea;
+    }
+}
+
