482f355b44e08a3125d51a79c3e44ee00538f451
shreemankumar <bshreemankumar@gmail.com>
2023-10-30 23:32:52 +0530

    Update Largest_Rectangle _in_Histogram.java

--- Leetcode/stack/Largest_Rectangle _in_Histogram.java
+++ Leetcode/stack/Largest_Rectangle _in_Histogram.java
@@ -59,3 +59,52 @@ class Solution {
     }
 }
 
+
+
+//method 2
+
+class Solution {
+     public int[] getNSELI(int[] arr, int n) {
+        int[] nseli = new int[n];
+        int[] nseri = new int[n];
+        Stack<Integer> st = new Stack<>();
+
+        for (int i = n - 1; i >= 0; i--) {
+            int ele = arr[i];
+
+            while (!st.isEmpty() && arr[st.peek()] > ele) {
+                int idx=t.peek();
+                nseli[idx] = i;
+                st.pop();
+            }
+            st.push(i);
+        }
+           if(!st.isEmpty()){
+                nseli[idx] = i;
+            }
+            else{
+                 nseli[idx] = n;
+            }
+
+        while (!st.isEmpty()) {
+            nseli[st.peek()] = -1;
+            st.pop();
+        }
+        if(!st.isEmpty()){
+                nseli[idx] = i;
+            }
+            else{
+                 nseli[idx] = n;
+            }
+              int maxarea = 0;
+        for (int i = 0; i < n; i++) {
+            int height = heights[i];
+            int width = nseri[i] - nseli[i] - 1;
+            int area = height * width;
+            maxarea = Math.max(maxarea, area);
+        }
+        return maxarea;
+        
+    }
+}
+
