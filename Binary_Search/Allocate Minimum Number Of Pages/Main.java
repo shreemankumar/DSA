09054c542ca625d7ae65de3b8666d4b090fc196d
shreemankumar <bshreemankumar@gmail.com>
2023-12-01 23:58:34 +0530

    updated

---
+++ Binary_Search/Allocate Minimum Number Of Pages/Main.java
@@ -0,0 +1,28 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Solution {
+    public long MinimumPages(int[] A, int B) {
+        //Write your code here
+        
+    }
+}
+
+class Main {
+    
+
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+	    
+	    int N = sc.nextInt();
+	    int B = sc.nextInt();
+        int[] A = new int[N];
+        for(int i=0;i<N;i++)
+            A[i] = sc.nextInt();
+
+        Solution Obj = new Solution(); 
+        System.out.println(Obj.MinimumPages(A,B));
+	}
+}
