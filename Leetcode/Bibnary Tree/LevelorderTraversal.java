1a8b8fe4d209b78976122a895b0edb66fc69ce73
shreemankumar <bshreemankumar@gmail.com>
2023-12-17 23:57:52 +0530

    Leetcode 102

---
+++ Leetcode/Bibnary Tree/LevelorderTraversal.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        if (root == null) {
+            return result;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> currentLevel = new ArrayList<>();
+
+            for (int i = 0; i < size; i++) {
+                TreeNode rnode = queue.remove();
+                currentLevel.add(rnode.val);
+
+                if (rnode.left != null) {
+                    queue.add(rnode.left);
+                }
+
+                if (rnode.right != null) {
+                    queue.add(rnode.right);
+                }
+            }
+
+            result.add(currentLevel);
+        }
+
+        return result;
+    }
+}
