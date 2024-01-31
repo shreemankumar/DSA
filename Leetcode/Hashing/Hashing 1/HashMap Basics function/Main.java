f8dcf63987df83b3a6ef5f3e17c38fd54c84e2e4
shreemankumar <bshreemankumar@gmail.com>
2024-02-01 01:42:15 +0530

    Basics functions of HashMap

---
+++ Leetcode/Hashing/Hashing 1/HashMap Basics function/Main.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        HashMap<Integer, String> mymap = new HashMap<>();
+        // add key,value pair in map
+        mymap.put(12, "ram");
+         mymap.put(2, "sita");
+        
+        // print these values or see them 
+        // how to loop around 
+        
+        // in syntax : you get an entry set (is a key value player) = which is stored in map.entery 
+        // and this loop is foreach loop , here 'm' is a variable . and from this variable u can get key and value IllegalAccessError
+        // instread of Map.Entry , we can use var ,(which is a datatype in case of when we don't know the data type it is then we use var )
+        // and it is automatically convert in to the exact datatpe that u want to that
+        
+        
+        // for (Map.Entry m : mymap.entrySet()) {    // Map.entry is like a datatype 
+        //     System.out.print(m.getKey() + ",");
+        //     System.out.println(m.getValue());
+        // }
+        
+        // remove 
+        
+        //mymap.remove(2,"sita");
+        //mymap.remove(2);
+        // for(var m : mymap.entrySet()){
+        //     System.out.print(m.getKey()+",");
+        //     System.out.println(m.getValue());
+        // }
+        
+        // loop 
+        for(int key : mymap.keySet()){
+            System.out.print(key +",");
+            System.out.println(mymap.get(key));
+        }
+    }
+}
