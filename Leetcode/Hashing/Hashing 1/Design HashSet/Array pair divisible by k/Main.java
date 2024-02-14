d115b9d809c4447b0846a422e2270f358aebca83
shreemankumar <bshreemankumar@gmail.com>
2024-02-14 13:44:01 +0530

    updated

--- Leetcode/Hashing/Hashing 1/Design HashSet/Array pair divisible by k/Main.java
+++ Leetcode/Hashing/Hashing 1/Design HashSet/Array pair divisible by k/Main.java
@@ -3,25 +3,10 @@ import java.util.*;
 
 class Solution {
     public boolean arrayPairs(int[] arr, int k) {
-		if(arr.length % 2 != 0) return false;
-		HashMap<Integer, Integer> remFreqMap = new HashMap<>();
-		// making the rem with freq
-		for(int a: arr){
-			int rem = a % k;
-			remFreqMap.put(rem, remFreqMap.getOrDefault(rem, 0) +1);
-		}
+		// create a frequency map for a hashmap
 
-		
-		for(int rem : remFreqMap.keySet()){
-			int CompRem = k - rem;
-			if(rem == 0){
-				// continue;
-			}
-			else if(remFreqMap.get(rem) != remFreqMap.get(CompRem)){
-				return false;
-			}
-		}
-		return true;
+        HashMap<Integer,Integer> freqMap = new HashMap<>();
+        
     }
 }
 
